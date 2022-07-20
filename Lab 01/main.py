c151ec7f1040669de2f032f647919bfe4708c9ff
Gabriela Paola Contreras Guerra <60274435+Paola-Contreras@users.noreply.github.com>
2022-07-19 18:54:22 -0600

    Draw Polygon

---
+++ Lab 01/main.py
@@ -0,0 +1,61 @@
+#Universidad del Valle de Guatemala
+#Graficos por computador - S20 
+#Gabriela Paola Contreras Guerra
+#LAB 01 -- Fill polgygon
+
+from poligonos import Renderer, color, V2
+
+w = 900
+h = 500
+
+rend = Renderer (w,h)
+
+#Arrays of points 
+
+P1 = [V2(165, 380), V2(185, 360), V2(180, 330), V2(207, 345),
+      V2(233, 330), V2 (230, 360), V2(250, 380), V2(220, 385),
+      V2(205, 410), V2(193, 383)]
+
+P2 = [V2(321, 335), V2(288, 286), V2(339, 251), V2(374, 302)]
+
+P3 = [V2(377, 249), V2(411, 197), V2(436, 249)]
+
+P4 = [V2(413, 177), V2(448, 159), V2(502, 88), V2(553, 53),
+      V2(535, 36), V2(676, 37), V2(660, 52), V2(750, 145),
+      V2(761, 179), V2(672, 192), V2(659, 214), V2(615, 214), 
+      V2(632, 230), V2(580, 230), V2(597, 215), V2(552, 214),
+      V2(517, 144), V2(466, 180)]
+
+P5 = [V2(682, 175), V2(708, 120), V2(735, 148), V2(739, 170)]
+
+
+#Function to draw a polygon 
+def draw (polygon, clr):
+    for i in range (len(polygon)):
+        rend.glLine(polygon[i], polygon[(i + 1) % len(polygon)], clr)
+
+#Draw each polygon 
+draw(P1, color(1,1,0))
+draw(P2, color(0,0.8,0.8))
+draw(P3, color(0,0.8,0.4))
+draw(P4, color(0.75,0.75,0.75))
+draw(P5, color(0.75,0.75,0.75))
+
+#Function to fill a polygon 
+def fill (x,y,bclr, clr):
+    c = getPixel(x,y)
+
+    if c != bclr or c != clr:
+        setPixel(x,y,clr)
+        fill(x+1, bclr, clr)
+        fill(x-1, bclr, clr)
+        fill(x,y+1, bclr, clr)
+        fill(x,y-1, bclr, clr)
+        
+
+
+#Fill polygons 
+fill()
+
+#GENEATE IMG 
+rend.glFinish("lab1.bmp")
